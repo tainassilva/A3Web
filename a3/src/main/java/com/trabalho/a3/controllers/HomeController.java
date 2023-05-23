7f9173ff61b1a2a2070821d621e7d1d6cba3ba5f
Taina SSantos Silva <tainassilva25111996@gmail.com>
2023-05-23 11:56:15 -0300

    Adicionando primeira parte do backend em que gera as tabelas no banco de dados e faz um teste com hello world no navegador

---
+++ a3/src/main/java/com/trabalho/a3/controllers/HomeController.java
@@ -0,0 +1,5 @@
+package com.trabalho.a3.controllers;
+
+ class HomeController {
+	
+}
---
+++ a3/src/test/java/com/trabalho/a3/A3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.a3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
