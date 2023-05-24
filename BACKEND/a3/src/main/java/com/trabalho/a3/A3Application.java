d4ea63c55c19f22bbff30e4080660afa069e9c63
Taina SSantos Silva <tainassilva25111996@gmail.com>
2023-05-24 11:33:47 -0300

    Arrumei o erro 404 mas agora temos um erro 500 kkkkk quem quiser se habilitar e arrumar esse bug vai dar bom

--- BACKEND/a3/src/main/java/com/trabalho/a3/A3Application.java
+++ BACKEND/a3/src/main/java/com/trabalho/a3/A3Application.java
@@ -2,10 +2,8 @@ package com.trabalho.a3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.bind.annotation.RestController;
 
-@EntityScan(basePackages = "model")
 @SpringBootApplication
 @RestController
 public class A3Application {
