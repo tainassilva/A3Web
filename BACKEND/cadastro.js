4bcb5886a4a7f24864f2638e746b315ec5443b53
Taina SSantos Silva <tainassilva25111996@gmail.com>
2023-06-15 10:27:06 -0300

    Ver o porquê do erro SQLITE_CANTOPEN para fazer o cadastro

--- BACKEND/cadastro.js
+++ BACKEND/cadastro.js
@@ -1,36 +1,46 @@
 const express = require('express');
 const path = require('path');
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./DATABASE/usuario.db');
-const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 
-const porta = 3333;
 const app = express();
-app.use(cors({ origin: '*' }));
+const port = 3333;
+const dbPath = path.join(__dirname, './DATABASE/usuario.db');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'cadastro.html'));
+// Configuração do banco de dados
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.message);
+  } else {
+    console.log('Conexão bem-sucedida ao banco de dados SQLite');
+  }
 });
 
-app.post('/add', function(req, res) {
-  console.log(req.body);
-  db.run(
-    'INSERT INTO usuario (nome, email, senha, cpf, celular) VALUES (?, ?, ?, ?, ?)',
-    [req.body.name, req.body.email, req.body.password, req.body.cpf, req.body.phone],
-    function(err) {
-      if (err) {
-        return console.log(err.message);
-      }
+
+
+
+// Rota para cadastrar usuário
+app.post('/add', (req, res) => {
+  const { name, email, password, cpf, phone } = req.body;
+
+  const insertQuery = `
+    INSERT INTO usuario (nome, email, senha, cpf, celular) VALUES (?, ?, ?, ?, ?)
+  `;
+
+  db.run(insertQuery, [name, email, password, cpf, phone], function(err) {
+    if (err) {
+      console.error('Erro ao inserir usuário:', err.message);
+      res.status(500).send('Erro ao cadastrar usuário');
+    } else {
       console.log('Novo usuário adicionado com sucesso');
-      res.send('Novo usuário com nome = ' + req.body.name);
+      res.redirect('/login.html');
     }
-  );
+  });
 });
 
-app.listen(porta, function() {
-  console.log('Servidor ouvindo na porta ' + porta);
+app.listen(port, () => {
+  console.log(`Servidor ouvindo na porta ${port}`);
 });
