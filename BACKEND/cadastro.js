e4cdd09e4d7879a2f66fba87580ce04a9fa13371
Taina SSantos Silva <tainassilva25111996@gmail.com>
2023-06-16 01:23:42 -0300

    teste

--- BACKEND/cadastro.js
+++ BACKEND/cadastro.js
@@ -19,6 +19,11 @@ const db = new sqlite3.Database(dbPath, (err) => {
   }
 });
 
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Rota para cadastrar usuário
 app.post('/add', (req, res) => {
   const { name, email, password, cpf, phone } = req.body;
@@ -44,77 +49,7 @@ app.post('/login', (req, res) => {
 
   const selectQuery = `
     SELECT * FROM usuario WHERE email = ? AND senha = ?
-  `;const express = require('express');
-  const path = require('path');
-  const sqlite3 = require('sqlite3').verbose();
-  const bodyParser = require('body-parser');
-  
-  const app = express();
-  const port = 3333;
-  const dbPath = path.join(__dirname, 'DATABASE/usuario.db');
-  
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  
-  // Configuração do banco de dados
-  const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-      console.error('Erro ao conectar ao banco de dados:', err.message);
-    } else {
-      console.log('Conexão bem-sucedida ao banco de dados SQLite');
-    }
-  });
-  
-  // Rota para cadastrar usuário
-  app.post('/add', (req, res) => {
-    const { name, email, password, cpf, phone } = req.body;
-  
-    const insertQuery = `
-      INSERT INTO usuario (nome, email, senha, cpf, celular) VALUES (?, ?, ?, ?, ?)
-    `;
-  
-    db.run(insertQuery, [name, email, password, cpf, phone], function(err) {
-      if (err) {
-        console.error('Erro ao inserir usuário:', err.message);
-        res.status(500).send('Erro ao cadastrar usuário');
-      } else {
-        console.log('Novo usuário adicionado com sucesso');
-        res.redirect('/login.html');
-      }
-    });
-  });
-  
-  // Rota para realizar o login
-  app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-  
-    const selectQuery = `
-      SELECT * FROM usuario WHERE email = ? AND senha = ?
-    `;
-  
-    db.get(selectQuery, [email, password], (err, row) => {
-      if (err) {
-        console.error('Erro ao consultar usuário:', err.message);
-        res.status(500).send('Erro ao realizar o login');
-      } else if (row) {
-        console.log('Login bem-sucedido');
-        res.redirect('/agenda.html');
-      } else {
-        console.log('Credenciais inválidas');
-        res.redirect('/login.html');
-      }
-    });
-  });
-  
-  // Rota para a página de login
-  app.get('/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/login.html'));
-  });
-  
-  app.listen(port, () => {
-    console.log(`Servidor ouvindo na porta ${port}`);
-  });
-  
+  `;
 
   db.get(selectQuery, [email, password], (err, row) => {
     if (err) {
@@ -130,7 +65,11 @@ app.post('/login', (req, res) => {
   });
 });
 
+// Rota para a página de login
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/login.html'));
+});
+
 app.listen(port, () => {
   console.log(`Servidor ouvindo na porta ${port}`);
 });
-
--- BACKEND/login.js
+++ BACKEND/login.js
@@ -1,35 +1,35 @@
-//ouvinte de evento para o evento "submit" do formulário. 
-//Dentro desse ouvinte de evento, a propriedade checkValidity() do formulário verifica se todos 
-//os campos obrigatórios estão preenchidos. Se o formulário for inválido, 
-//chamamos event.preventDefault() para cancelar o envio do formulário.
+// //ouvinte de evento para o evento "submit" do formulário. 
+// //Dentro desse ouvinte de evento, a propriedade checkValidity() do formulário verifica se todos 
+// //os campos obrigatórios estão preenchidos. Se o formulário for inválido, 
+// //chamamos event.preventDefault() para cancelar o envio do formulário.
 
-document.getElementById("login-form").addEventListener("submit", function(event) {
-  event.preventDefault();
+// document.getElementById("login-form").addEventListener("submit", function(event) {
+//   event.preventDefault();
 
-  var loginForm = document.getElementById("login-form");
+//   var loginForm = document.getElementById("login-form");
 
-  if (loginForm.checkValidity()) {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+//   if (loginForm.checkValidity()) {
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
 
-    var usuariosCadastrados = [ //aqui tem que conectar no banco de dados, pra deixar a quantidade de acordo com o banco de dados, no NODE imagino
-      { username: "usuario1", password: "senha1" },
-      { username: "usuario2", password: "senha2" },
-      { username: "usuario3", password: "senha3" }
-    ];
+//     var usuariosCadastrados = [ //aqui tem que conectar no banco de dados, pra deixar a quantidade de acordo com o banco de dados, no NODE imagino
+//       { username: "usuario1", password: "senha1" },
+//       { username: "usuario2", password: "senha2" },
+//       { username: "usuario3", password: "senha3" }
+//     ];
 
-    var usuarioEncontrado = usuariosCadastrados.find(function(usuario) {
-      return usuario.username === username && usuario.password === password;
-    });
+//     var usuarioEncontrado = usuariosCadastrados.find(function(usuario) {
+//       return usuario.username === username && usuario.password === password;
+//     });
 
-    if (usuarioEncontrado) {
-      window.location.href = "cadastro.html";
-    } else {
-      alert("Usuário ou senha inválidos");
-    }
-  } else {
-    alert("Por favor, preencha todos os campos corretamente");
-  }
-});
+//     if (usuarioEncontrado) {
+//       window.location.href = "agenda.html";
+//     } else {
+//       alert("Usuário ou senha inválidos");
+//     }
+//   } else {
+//     alert("Por favor, preencha todos os campos corretamente");
+//   }
+// });
 
 
